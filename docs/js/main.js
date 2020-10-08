3d86daa894625d541a69756f7e3421773e14c32e
khasenovweb <hasenovkirill93@gmail.com>
2020-10-08 18:37:25 +0500

    animate

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,20 @@
  $(document).ready(function(){
- 	BackgroundCheck.init({
- 	    targets: '.target'
- 	  });
+ 	$(window).scroll(function() {
+ 		//==============Смена цвета гамбургера==============//
+ 		var scrollTop = $(window).scrollTop();
+ 		var offsetMenu = $('.header__menu').offset().top;
+ 		var heroHeight = $('.hero').height();
+ 		if(offsetMenu > heroHeight) {
+ 			$('.header__menu').addClass('black'); 
+ 		}else {
+ 			$('.header__menu').removeClass('black'); 
+ 		}
+ 		//==============Смена цвета гамбургера==============//
+
+ 		
+ 		$('.hero').bgscroll();
+ 		// $('.hero').css('background-size', '10' + parseInt(scrollTop / 70, 0) + '% ');
+	});
+	
+	new WOW().init();
  });
